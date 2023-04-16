8c2166e602b2d3261b6553f0fbb7f4ee7a1613ad
Wael Isa <24850589+waelisa@users.noreply.github.com>
2023-04-16 20:45:51 +0300

    clean-up

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# GetFreeOS-plasma
-Build GetFreeOS plasma - Arch
+# arcolinuxl-iso
 
-##
-
-git clone https://github.com/waelisa/GetFreeOS-plasma.git
-
-cd GetFreeOS-plasma
-
-./GetFreeOS-build-the-iso.sh
-
-##
-
-Note:- script work in your home folder.
-
-Thanks to Erik Dubois for the great work.
+# stable
--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -3,7 +3,6 @@
 
 iso_name="GetFreeOS-plasma"
 iso_label="GetFreeOS-plasma-$(date +%Y.%m.%d)"
-#iso_label="GetFreeOS-plasma-$(date +%Y.%m)"
 iso_publisher="GetFreeOS <https://GetFreeOS.com>"
 iso_application="GetFreeOS Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
@@ -17,15 +16,12 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/root"]="0:0:750"
+  ["/etc/gshadow"]="0:0:600"
+  ["/etc/shadow"]="0:0:600"
+  ["/root"]="0:0:700"
   ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
-  ["/usr/local/bin/getfreeos-make-a-pure-arch"]="0:0:755"
-  ["/usr/local/bin/getfreeos-displaymanager-check"]="0:0:755"
-  ["/usr/local/bin/fix-keys"]="0:0:755"
+  ["/etc/grub.d/40_custom"]="0:0:755"
+  ["/usr/local/bin/arcolinux-snapper"]="0:0:755"
 )
